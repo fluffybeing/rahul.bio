4399462b065caf86fc7527aea3b5a2cd380cb0dc
Rahul Ranjan <137078342+rahulflir@users.noreply.github.com>
2024-07-27 01:41:29 +0200

    show published date in the blog list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,7 @@
+import { ArticleTags } from 'app/components/tags';
 import { getBlogPosts } from 'app/db/blog';
 import { getViewsCount } from 'app/db/queries';
+import { formatDate } from 'app/og/utils';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import ViewCounter from '../components/view-counter';
@@ -32,11 +34,19 @@ export default function BlogPage() {
             className="flex flex-col space-y-1 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col">
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
-              <Suspense fallback={<p className="h-6" />}>
+            <div className="w-full flex flex-col text-neutral-900 dark:text-neutral-100">
+              <p className="tracking-tight">{post.metadata.title}</p>
+              {/* <p className="text-xs">
+                <ArticleTags tags={post.metadata.tags} />
+              </p> */}
+            </div>
+            <div className="flex justify-between items-center tracking-tight max-w-[480px]">
+              <Suspense fallback={<p className="h-5" />}>
+                <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                  {formatDate(post.metadata.publishedAt)}
+                </p>
+              </Suspense>
+              <Suspense fallback={<p className="h-5" />}>
                 <Views slug={post.slug} />
               </Suspense>
             </div>
--- app/components/tags.tsx
+++ app/components/tags.tsx
@@ -1,1 +1,32 @@
 import React from 'react';
+
+interface ArticleTagsProps {
+  tags: string | string[]; // Tags can be a string or an array of strings
+}
+
+function isArrayEmpty(arr: any[]): boolean {
+  return arr.length === 0;
+}
+
+export function ArticleTags({ tags }: ArticleTagsProps) {
+  if (
+    !tags ||
+    (typeof tags === 'string' && tags.trim() === '') ||
+    (Array.isArray(tags) && isArrayEmpty(tags))
+  ) {
+    return null;
+  }
+
+  const tagArray =
+    typeof tags === 'string' ? tags.split(',').map((t) => t.trim()) : tags;
+
+  return (
+    <div className="flex items-center justify-center">
+      {tagArray.map((tag, i) => (
+        <span key={i} className="">
+          {tag}
+        </span>
+      ))}
+    </div>
+  );
+}
