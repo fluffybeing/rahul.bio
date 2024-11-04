efe0aedb3e8c6f1eb20cb3254ae1d1f8b812a557
Rahul Ranjan <137078342+rahulflir@users.noreply.github.com>
2024-11-04 19:38:08 +0100

    Update google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter, Roboto_Mono } from 'next/font/google';
@@ -72,7 +72,7 @@ export default function RootLayout({
     >
       <head>
         <SandpackCSS />
-        <GoogleTagManager gtmId="G-95G2YZC86T" />
+        <GoogleTagManager gtmId="GTM-NJ29XH4Q" />
       </head>
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto text-wrap:pretty">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
@@ -81,6 +81,7 @@ export default function RootLayout({
           <SpeedInsights />
         </main>
       </body>
+      <GoogleAnalytics gaId="G-CNTLYDV06F" />
     </html>
   );
 }
